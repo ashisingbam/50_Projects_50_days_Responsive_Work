954fd0aba7b1d929a7827fa6dc3da6c397e71a6c
ashisingbam <ashisinghbam@example.com>
2023-04-14 17:49:54 +0530

    code optimised

--- 03_ROTATING_NAVIGATION_ANIMATION/script.js
+++ 03_ROTATING_NAVIGATION_ANIMATION/script.js
@@ -1,20 +1,10 @@
-
 const container = document.querySelector('.container')
-const circleMenu = document.querySelector('.circle')
 const openMenu = document.querySelector('#open')
 const closeMenu = document.querySelector('#close')
-const navLi = document.querySelectorAll('.nav-icons')
 
-function handleEvents() {
-    circleMenu.classList.toggle('rotateIcon')
-    container.classList.toggle('rotatePage')
-    navLi.forEach(li => {
-      li.classList.toggle('show-nav')
-    })
-}
 openMenu.addEventListener('click', () => {
-  handleEvents()
+  container.classList.toggle('show-nav')
 })
 closeMenu.addEventListener('click', () => {
-  handleEvents()
+  container.classList.toggle('show-nav')
 })
